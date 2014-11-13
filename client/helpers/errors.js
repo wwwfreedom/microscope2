e19984ef4bd81ebfbc43b4169b9fe8f49ed9ce26
Kevin <tig3r86@gmail.com>
2014-11-13 10:23:26 +0700

    9-1.1 fix typo Basic error reporting.

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -1,5 +1,5 @@
 // Local (client-only) collection
-Errors = new Meteor.Collection(null);
+Errors = new Mongo.Collection(null);
 
 throwError = function  (message) {
   Errors.insert({message: message})
