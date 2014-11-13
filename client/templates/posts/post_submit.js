fb70549adcf81ea1c9ceeb21fd2777291ad2f0e1
Kevin <tig3r86@gmail.com>
2014-11-13 14:35:33 +0700

    9-4 Validate post contents on submision

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,17 @@
+Template.postSubmit.created = function() {
+  Session.set('postSubmitErrors', {});
+}
+
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function(field) {
+    //the !!force the the Session.get to become a boolean so if Session.get('postSubmitErrors') is true then has-error otherwise just a empty space
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postSubmit.events({
   'submit form': function (e) {
     e.preventDefault();
@@ -6,6 +20,12 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
+
+    //set the session with validation error
+    var errors = validatePost(post);
+    if(errors.title || errors.url) {
+      return Session.set('postSubmitErrors', errors);
+    }
 
     Meteor.call('postInsert', post, function (error, result) {
       //display the error to the user and abort
