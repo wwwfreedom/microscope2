bef96fd4c6c64c0766b4117df767717f44e80fe3
Kevin <tig3r86@gmail.com>
2014-11-14 10:31:55 +0700

    10-4 Made a simple publication/subscription for comments.

--- client/templates/comments/comment_submit.js
+++ client/templates/comments/comment_submit.js
@@ -15,16 +15,18 @@ Template.commentSubmit.events({
   'submit form': function (e, template) {
     e.preventDefault();
 
+    //name=body is refering to the textarea with name="body" in comment_submit.html using jquery to find and select the textarea
     var $body = $(e.target).find('[name=body]');
     var comment = {
       body: $body.val(),
+      //need to find out what is template.data._id is refering to
       postId: template.data._id
     };
 
     var errors = {};
     if (! comment.body) {
       errors.body = "Please write some content";
-      return Session.set(commentSubmitErrors, errors);
+      return Session.set('commentSubmitErrors', errors);
     }
 
     Meteor.call('commentInsert', comment, function (error, commentId) {
--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,8 @@ Meteor.publish('posts', function () {
   return Posts.find();
 });
 
-Meteor.publish('comments', function () {
-  return Comments.find();
+Meteor.publish('comments', function (postId) {
+  //only publish comment for a specific postId
+  check(postId, String)
+  return Comments.find({postId: postId});
 });
