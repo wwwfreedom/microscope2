9cff9c453827a66067d7a435f7cb0a0266f8efef
Kevin <tig3r86@gmail.com>
2014-11-14 15:56:49 +0700

    11-2 Display notifications in the header.

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -3,6 +3,7 @@ Template.postItem.helpers({
     return this.userId === Meteor.userId();
   },
 
+  // need a more comprehensive capture of the domain because the bug at the moment will only display {{domain}} if it was enter as an address starting with http:// might have to modify to use some regular expression to extract the host name for user input that doesn't start with http:// or can even add if http:// if the user in put doesnt have it.
   domain: function () {
     var a = document.createElement('a');
     a.href = this.url;
